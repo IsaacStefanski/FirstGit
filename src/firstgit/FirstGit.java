251644550105a4f9ec710cc5a7d0c515499f53d9
Isaac <Isaac@ISAAC_HP_OMEN>
2017-08-29 15:41:14 -0500

    Added second comment in main

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -11,6 +11,7 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Second comment");
     }
     
 }
